fa854bf534277c5a8c60dac598b7b69c26bcf1ea
hemg2 <hemg2@naver.com>
2022-03-21 00:00:57 +0900

    포켓몬 테이블 구성 155개

---
+++ week2/PokemonTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  PokemonTableViewCell.swift
+//  week2
+//
+//  Created by 1 on 2022/03/20.
+//
+
+import UIKit
+
+class PokemonTableViewCell: UITableViewCell {
+    @IBOutlet weak var pokemonimageview: UIImageView!
+    @IBOutlet weak var pokemonnamelable: UILabel!
+    @IBOutlet weak var indextile: UILabel!
+    @IBOutlet weak var pokemonyupe: UILabel!
+}
--- week2/ViewController.swift
+++ week2/ViewController.swift
@@ -7,13 +7,36 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
+    @IBOutlet weak var tablView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 155
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if indexPath.row == 0 {
+            var cell = tableView.dequeueReusableCell(withIdentifier: "Pokemoncellforrow", for: indexPath)as! PokemonTableViewCell
+            cell.pokemonnamelable.text = "피카츄"
+            cell.indextile.text = "025"
+            cell.pokemonyupe.text = "전기타입"
+            return cell
+        }
+        else if indexPath.row == 1 {
+            var cell = tableView.dequeueReusableCell(withIdentifier: "Pokemoncellforrow", for: indexPath)as! PokemonTableViewCell
+            cell.pokemonnamelable.text = "꼬북이"
+            cell.indextile.text = "033"
+            cell.pokemonyupe.text = "전기타입"
+            return cell
+        }
+        else {
+            var cell = tableView.dequeueReusableCell(withIdentifier: "Pokemoncellforrow", for: indexPath)as! PokemonTableViewCell
+            cell.pokemonnamelable.text = "양유기"
+            cell.indextile.text = "0000"
+            cell.pokemonyupe.text = "타입"
+            return cell
+        }
+    }
 }
-
